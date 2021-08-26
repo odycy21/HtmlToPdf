f8157f204dcbbfae0d1b4358230d09889cbd62c7
bryan osal <bryanosal@gmail.com>
2021-08-26 12:28:44 +0800

    First Commit

---
+++ assets/script/script.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'invoice.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
+
+
+
+// Get Current Date
+
+
+function getDate() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+
+
+    if(month.toString().length == 1) {
+         month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    }   
+
+    var date = year+'/'+month+'/'+day;   
+     return date;
+}
+
+// 
+setInterval(function(){
+    currentDate = getDate();
+    document.getElementById("currDate").innerHTML = currentDate;
+}, 1000);
